072b17e479efe253f08a490b700d6ba92447bf47
dandesonjew <danieltanderson@berkeley.edu>
2017-04-28 14:13:46 -0700

    refractored implementation ofbuttonView

--- client/js/app.js
+++ client/js/app.js
@@ -1,40 +1,11 @@
 const TableModel = require ('./table-model');
 const TableView = require('./table-view');
+const ButtonView = require('./button-view');
 
 const model = new TableModel();
 const tableView = new TableView(model);
 
 tableView.init()
 
-
-class ViewManager {
-	constructor(model, tableView) {
-		this.model = model;
-		this.view = tableView;
-	}
-
-	connectEventHandlers() {
-		document.getElementById('add-col')
-		.addEventListener(
-			'click',
-			this.addCol.bind(this));
-		document.getElementById('add-row')
-		.addEventListener(
-			'click',
-			this.addRow.bind(this));
-	}
-
-	addCol(event){
-		event.preventDefault();
-		this.model.numCols += 1;
-		this.view.renderTable();
-
-	}
-	addRow(event){
-		event.preventDefault();
-		this.model.numRows += 1;
-		this.view.renderTable();
-	}
-}
-const viewManager = new ViewManager(model, tableView);
-viewManager.connectEventHandlers();
+const buttonView = new ButtonView(model, tableView);
+buttonView.connectEventHandlers();
