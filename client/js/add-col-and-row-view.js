1a7c8eb702314d854043b8326d06753eee99a903
dandesonjew <danieltanderson@berkeley.edu>
2017-05-03 16:05:57 -0700

    incomplete works of adding a new row/col

--- client/js/add-col-and-row-view.js
+++ client/js/add-col-and-row-view.js
@@ -17,12 +17,28 @@ class AddColAndRowView {
 
   addCol(event){
     event.preventDefault();
+    console.log(this.view.currentColLocation);
+    // if(this.view.currentColLocation) {
+    //   for (var i = 0; i < this.model.data.length; i++) {
+    //     if (this.model.data[i][0] > this.view.currentColLocation) {
+    //       this.model.data[i][0] += 1;
+    //     }
+    //   }
+    // }
     this.model.numCols += 1;
     this.view.renderTable();
 
   }
   addRow(event){
     event.preventDefault();
+    console.log(this.view.currentCellLocation);
+    console.log(this.model.data);
+    console.log(this.model.data);
+    for (var i = 0; i < this.model.data.length; i++) {
+      if (this.model.data[i][0] == this.model.numRows) {
+          this.model.data[i][0] += 1;
+        }
+    }
     this.model.numRows += 1;
     this.view.renderTable();
   }
